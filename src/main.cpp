47f18eb0efbfe254bf42e9680c252bca0f3bc52d
Tor Stensola <StensolaTor@Tors-MBP.lan>
2018-03-18 11:02:16 +0000

    Test

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+unsigned long time;
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.println("Hello Computer");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+    Serial.print("Time: ");
+    time = micros();
+    Serial.println(time);
+    Serial.println("Test");
+}
