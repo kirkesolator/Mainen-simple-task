7ef28f297b7e09f975790e2c32012a1b76fabbd2
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-03-22 16:31:24 +0000

    Added switch case structure and multiple pin outputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
 #include <Arduino.h>
 
+// Pin indentities
 int pokePin = 9;
-int val = 1;
+int rewardLpin = 13;
+int rewardRpin = 13;
+int odorPin = 13;
+int ledLpin = 13;
+int ledRpin = 13;
+
+// Various intitial conditions
+int val = 1; // Initial poke state
 int valO;
-int pokeNum = 0;
+int pokeNum = 0; // Poke counter
 int timer;
+int state = 0;
+int nextState;
+int ITI = 2000; // Intertrial interval
+
+// Timer variables
+int timerStart;
+int timeWait;
+int timeNow;
+bool doTimer;
+
+// Block variables
+int nTrials = 20;
+
 //---------------------------------SETUP----------------------------------------
 
 void setup() {
-    // put your setup code here, to run once:
     Serial.begin(9600);
     pinMode(pokePin, INPUT);
+    pinMode(rewardLpin, OUTPUT);
+    pinMode(rewardRpin, OUTPUT);
+
     while (! Serial);
     Serial.println("Script running");
 }
@@ -22,10 +45,33 @@ void loop() {
     valO = val; // store previous poke state
     val = digitalRead(pokePin); // record new poke state
 
-    // check for poke entry:
+    //..................Poke Detection..................
     if (val-valO < 0) {
       pokeNum = pokeNum + 1; // Update poke counter
       Serial.print("Poke #"); // Print to serial monitor
       Serial.println(pokeNum);
     }
+
+    //..................Global Timer..................
+    if (doTimer){
+      timeNow = millis();
+      if (timeNow - timerStart >= timeWait) {
+        state = nextState;
+        doTimer = 0;
+      }
+    }
+
+    //..................Trial State Progression..................
+    switch (state) {
+    case 0: // ITI
+      timeWait = ITI;
+      timerStart = millis();
+      doTimer = true;
+      nextState = 1;
+      Serial.print("State 0: ITI"); // Print state to serial monitor
+      break;
+    case 1: // Odor delivery
+
+      break;
+  }
 }
