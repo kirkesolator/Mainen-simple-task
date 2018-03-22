b079866433183407c1d878ad1a7bed1efab3e074
stensolator <tor.stensola@neuro.fchampalimaud.org>
2018-03-22 13:43:08 +0000

    Added text prompt of poke number

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,31 @@
 #include <Arduino.h>
 
 int pokePin = 9;
-int val;
+int val = 1;
 int valO;
 int pokeNum = 0;
 int timer;
-
-/*
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-*/
+//---------------------------------SETUP----------------------------------------
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
     pinMode(pokePin, INPUT);
+    while (! Serial);
     Serial.println("Script running");
 }
 
-/*
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-----------------------------------------------------------------------------
-*/
+//----------------------------------LOOP----------------------------------------
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    // Read and compare poke values
     valO = val; // store previous poke state
     val = digitalRead(pokePin); // record new poke state
-    // delay(50);
-    if (val-valO < 0)
-    {
-      pokeNum = pokeNum + 1;
+
+    // check for poke entry:
+    if (val-valO < 0) {
+      pokeNum = pokeNum + 1; // Update poke counter
+      Serial.print("Poke #"); // Print to serial monitor
       Serial.println(pokeNum);
-      // timer = millis();
-      // while (millis() - timer < 50) {
-      //   // Waiting...
-      // }
     }
 }
